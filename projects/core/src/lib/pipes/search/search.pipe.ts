3b62f8e66c79ff1f84927ac98d189389f7f2c17f
Angelo Raso <angeloraso6@gmail.com>
2025-04-05 15:34:44 -0300

    Fix search pipe

--- projects/core/src/lib/pipes/search/search.pipe.ts
+++ projects/core/src/lib/pipes/search/search.pipe.ts
@@ -41,19 +41,28 @@ export class BizySearchPipe implements PipeTransform {
 
     const getFn = (item: T, keys: Array<string>) => {
       const value = keys.reduce((acc, key) => acc && acc[key], item);
-      return typeof value === 'string' ? this.#removeAccentsAndDiacritics(value) : value;
+      return typeof value === 'string' ? this.#removeAccentsAndDiacritics(value) : String(value);
+    }
+
+    const isNumber = (number: unknown): number is number => {
+      const regex = /^[0-9]*$/;
+      return regex.test(String(number).toLowerCase());
+    }
+
+    const isEmail = (email: unknown): boolean => {
+      const regex = /^(([^ñ<>()[\]\\.,;:\s@"]+(\.[^ñ<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regex.test(String(email).toLowerCase());
     }
 
     // Remove empty items
     search = search.filter(n => n);
     search.forEach(_keyword => {
-      // Apply perfect match if "search" is a number or is an email
-      const searchIsText = isNaN(Number(_keyword)) && !String(_keyword).includes('@');
 
-      if (searchIsText) {
+      if (!isNumber(_keyword) && !isEmail(_keyword)) {
         this.searchPipeOptions = new BizySearchPipeOptions({...options, getFn}, _keys);
         this.fuse = new Fuse(items, this.searchPipeOptions);
       } else {
+         // Apply perfect match if "search" is a number or is an email
         this.searchPipeOptions = new BizySearchPipeOptions({...options, ...this.perfectMatch, getFn}, _keys);
         this.fuse = new Fuse(items, this.searchPipeOptions);
       }
