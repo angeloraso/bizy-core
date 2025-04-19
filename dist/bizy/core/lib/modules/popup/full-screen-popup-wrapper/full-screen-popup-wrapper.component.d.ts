11e7b2986969347c58f207ad55448ea1c235ccae
Angelo Raso <angeloraso6@gmail.com>
2025-04-19 13:14:06 -0300

    Version 19.10.1

--- dist/bizy/core/lib/modules/popup/full-screen-popup-wrapper/full-screen-popup-wrapper.component.d.ts
+++ dist/bizy/core/lib/modules/popup/full-screen-popup-wrapper/full-screen-popup-wrapper.component.d.ts
@@ -3,9 +3,10 @@ import * as i0 from "@angular/core";
 export declare class BizyFullScreenPopupWrapperComponent<T> {
     #private;
     dynamicComponentContainer: ViewContainerRef;
+    loading: boolean;
     ngAfterViewInit(): void;
     loadDynamicComponent(): void;
-    close(): void;
+    close(): Promise<void>;
     static ɵfac: i0.ɵɵFactoryDeclaration<BizyFullScreenPopupWrapperComponent<any>, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<BizyFullScreenPopupWrapperComponent<any>, "bizy-full-screen-popup-wrapper", never, {}, {}, never, never, true, never>;
 }
