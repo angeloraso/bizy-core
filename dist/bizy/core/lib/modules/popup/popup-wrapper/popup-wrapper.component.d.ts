49908ebf334b776cebd78afec8008f5eb34b8f10
Angelo Raso <angeloraso6@gmail.com>
2025-04-19 13:39:26 -0300

    Version 19.10.3

--- dist/bizy/core/lib/modules/popup/popup-wrapper/popup-wrapper.component.d.ts
+++ dist/bizy/core/lib/modules/popup/popup-wrapper/popup-wrapper.component.d.ts
@@ -3,9 +3,10 @@ import * as i0 from "@angular/core";
 export declare class BizyPopupWrapperComponent<T> {
     #private;
     dynamicComponentContainer: ViewContainerRef;
+    disabled: boolean;
     ngAfterViewInit(): void;
     loadDynamicComponent(): void;
-    close(): void;
+    close(): Promise<void>;
     static ɵfac: i0.ɵɵFactoryDeclaration<BizyPopupWrapperComponent<any>, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<BizyPopupWrapperComponent<any>, "bizy-popup-wrapper", never, {}, {}, never, never, true, never>;
 }
