c9af2c3fb916300a87c02e6fa244dd617c14e586
Angelo Raso <angeloraso6@gmail.com>
2025-04-19 20:01:07 -0300

    Replace BizyUserAgentService with BizyDeviceService

---
+++ projects/core/src/lib/services/device/device.service.ts
@@ -0,0 +1,34 @@
+import { inject, Injectable } from '@angular/core';
+import { getUserAgent } from './uach-retrofill';
+import { DeviceDetectorService } from 'ngx-device-detector';
+
+@Injectable({ providedIn: 'root' })
+export class BizyDeviceService {
+  readonly #device = inject(DeviceDetectorService);
+
+  async getUserAgent() {
+    try {
+      const userAgent = await getUserAgent([
+        'architecture',
+        'bitness',
+        'model',
+        'platformVersion',
+        'uaFullVersion',
+        'fullVersionList'
+      ]);
+      return userAgent;
+    } catch {
+      return window.navigator.userAgent;
+    }
+  }
+
+  isMobile = (): boolean => this.#device.isMobile();
+
+  isTablet = (): boolean => this.#device.isTablet();
+
+  isDesktop = (): boolean => this.#device.isDesktop();
+
+  isPortrait = (): boolean => this.#device.orientation === 'portrait';
+
+  isLandscape = (): boolean => this.#device.orientation === 'landscape';
+}
--- projects/core/src/lib/services/services.module.ts
+++ projects/core/src/lib/services/services.module.ts
@@ -4,17 +4,17 @@ import { BizyViewportService } from './viewport.service';
 import { BizyKeyboardService } from './keyboard.service';
 import { BizyExportToCSVService } from './export-to-csv.service';
 import { BizyCacheService } from './cache.service';
-import { BizyUserAgentService } from './user-agent/user-agent.service';
 import { BizyValidatorService } from './validator.service';
 import { BizyStorageService } from './storage.service';
 import { BizyLogService } from './log.service';
 import { BizyRouterService } from './router.service';
 import { BizyCopyToClipboardService } from './copy-to-clipboard.service';
 import { BizyFormatSecondsService } from './format-seconds.service';
+import { BizyDeviceService } from './device/device.service';
 
 const SERVICES: Array<any> = [
   BizyAnimationService,
-  BizyUserAgentService,
+  BizyDeviceService,
   BizyCacheService,
   BizyCopyToClipboardService,
   BizyExportToCSVService,
