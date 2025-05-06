94ca7ea095de5077441c3dd3d08836ce8fcfe53d
Angelo Raso <angeloraso6@gmail.com>
2025-05-06 17:06:12 -0300

    Fix audio player

--- projects/core/src/lib/modules/audio-player/audio-player.component.ts
+++ projects/core/src/lib/modules/audio-player/audio-player.component.ts
@@ -1,5 +1,6 @@
+import { inject } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, Renderer2, Inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, Renderer2 } from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { MIME_TYPE } from './audio-player.types';
@@ -13,8 +14,10 @@ import { BizyButtonModule } from '../button';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BizyAudioPlayerComponent {
+  readonly #document = inject(DOCUMENT);
+  readonly #renderer = inject(Renderer2);
   @Input() id: string = `bizy-audio-player-${Math.random()}`;
-  @Input() mimeType: string = MIME_TYPE.MPEG;
+  @Input() mimeType: string;
   @Input() showDownload: boolean = true;
   @Input() autoplay: boolean = false;
   @Input() downloadURL: string;
@@ -28,28 +31,49 @@ export class BizyAudioPlayerComponent {
     }
 
     this._audioURL = audioURL;
-    this._audioRef = this._audioRef ?? this.document.getElementById(this.id) as HTMLAudioElement;
-    if (this._audioRef) {
-      this._audioRef.load();
+
+    if (!this.mimeType) {
+      const isOGG = this._audioURL.toLowerCase().includes('ogg');
+      if (isOGG) {
+        this.mimeType = MIME_TYPE.OGG;
+      } else {
+        const isWAV = this._audioURL.toLowerCase().includes('wav');
+        if (isWAV) {
+          this.mimeType = MIME_TYPE.WAV;
+        } else {
+          this.mimeType = MIME_TYPE.MPEG;
+        }
+      }
+    }
+
+    this.#audioRef = this.#document.getElementById(this.id) as HTMLAudioElement;
+
+    if (this.#audioRef) {
+      this.#audioRef.load();
 
       if (this.autoplay) {
-        this._audioRef.play();
-      }
+        this.#audioRef.play();
+      } 
+    } else {
+      const interval = setInterval(() => {
+        this.#audioRef = this.#document.getElementById(this.id) as HTMLAudioElement;
+        if (this.#audioRef) {
+          this.#audioRef.load();
+
+          if (this.autoplay) {
+            this.#audioRef.play();
+          }
+          clearInterval(interval);
+        }
+      }, 300);
     }
   }
 
-
-
   _audioURL: string | null = null;
-  _audioRef: HTMLAudioElement;
+  #audioRef: HTMLAudioElement;
   _playbackRate = 1;
   #trackPlaybackRate$ = new Subject<string>();
   #subscription = new Subscription();
-
-  constructor(
-    @Inject(DOCUMENT) private document: Document,
-    @Inject(Renderer2) private renderer: Renderer2
-  ) {}
 
   trackPlayerRate() {
     this.#subscription.add(this.#trackPlaybackRate$.pipe(
@@ -61,37 +85,40 @@ export class BizyAudioPlayerComponent {
   }
 
   _onTrackPlayerRate() {
-    this._audioRef = this._audioRef ?? this.document.getElementById(this.id) as HTMLAudioElement;
-    if (this._audioRef) {
-      switch (this._audioRef.playbackRate) {
+    if (!this.#audioRef) {
+      this.#audioRef = this.#document.getElementById(this.id) as HTMLAudioElement;
+    }
+
+    if (this.#audioRef) {
+      switch (this.#audioRef.playbackRate) {
         case 1:
-          this._audioRef.playbackRate = 1.5;
+          this.#audioRef.playbackRate = 1.5;
           this._playbackRate = 1.5;
           this.#trackPlaybackRate$.next('1.5');
           break;
         case 1.5:
-          this._audioRef.playbackRate = 2;
+          this.#audioRef.playbackRate = 2;
           this._playbackRate = 2;
           this.#trackPlaybackRate$.next('2');
           break;
         case 2:
-          this._audioRef.playbackRate = 1;
+          this.#audioRef.playbackRate = 1;
           this._playbackRate = 1;
           break;
         default:
-          this._audioRef.playbackRate = 1;
+          this.#audioRef.playbackRate = 1;
           this._playbackRate = 1;
       }
     }
   }
 
   _onDownload(): void {
-    const downloadButton = this.renderer.createElement('a');
-    this.renderer.setAttribute(downloadButton, 'download', this.downloadFileName);
-    this.renderer.setProperty(downloadButton, 'href', this.downloadURL);
-    this.renderer.appendChild(this.document.body, downloadButton);
+    const downloadButton = this.#renderer.createElement('a');
+    this.#renderer.setAttribute(downloadButton, 'download', this.downloadFileName);
+    this.#renderer.setProperty(downloadButton, 'href', this.downloadURL);
+    this.#renderer.appendChild(this.#document.body, downloadButton);
     downloadButton.click();
-    this.renderer.removeChild(this.document.body, downloadButton);
+    this.#renderer.removeChild(this.#document.body, downloadButton);
     this.onDownload.emit();
   }
 
