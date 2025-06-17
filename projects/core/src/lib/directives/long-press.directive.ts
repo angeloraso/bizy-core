0d743cc6b8178fadf181544115ad06cbd0623b4d
Angelo Raso <angeloraso6@gmail.com>
2025-06-17 19:39:44 -0300

    Adjust long press directive

--- projects/core/src/lib/directives/long-press.directive.ts
+++ projects/core/src/lib/directives/long-press.directive.ts
@@ -1,43 +1,46 @@
-import { Directive, ElementRef, EventEmitter, inject, Inject, Input, OnDestroy, Output } from '@angular/core';
-import { fromEvent, merge, of, Subscription, timer } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
+import {
+  Directive,
+  EventEmitter,
+  HostListener,
+  Output,
+  OnDestroy,
+  Input,
+} from '@angular/core';
 
 @Directive({
   selector: '[bizyLongPress]',
 })
 export class BizyLongPressDirective implements OnDestroy {
-  readonly #elementRef = inject(ElementRef);
   @Input() threshold = 500;
-  @Output() bizyLongPress = new EventEmitter<void>();
-  
-  #event: Subscription;
-  
-  constructor() {
-    const mousedown = fromEvent<MouseEvent>(this.#elementRef.nativeElement, 'mousedown').pipe(
-      filter((event) => event.button == 0), // Only allow left button (Primary button)
-      map(() => true) // turn on threshold counter
-    );
+  @Output() bizyLongPress = new EventEmitter<MouseEvent |TouchEvent>();
 
-    const touchstart = fromEvent(this.#elementRef.nativeElement, 'touchstart').pipe(map(() => true));
+  #pressTimeout: any = null;
 
-    const touchEnd = fromEvent(this.#elementRef.nativeElement, 'touchend').pipe(map(() => false));
-    
-    const mouseup = fromEvent<MouseEvent>(window, 'mouseup').pipe(
-      filter((event) => event.button == 0), // Only allow left button (Primary button)
-      map(() => false) // reset threshold counter
-    );
+  @HostListener('mousedown', ['$event'])
+  @HostListener('touchstart', ['$event'])
+  onPressStart(event: MouseEvent | TouchEvent): void {
+    this.clearTimeout(); // Clear any existing timeout
+    this.#pressTimeout = setTimeout(() => {
+      this.bizyLongPress.emit(event);
+    }, this.threshold);
+  }
 
-    this.#event = merge(mousedown, mouseup, touchstart, touchEnd)
-      .pipe(
-        switchMap(state => (state ? timer(this.threshold, 100) : of(null))),
-        filter(value => Boolean(value))
-      )
-      .subscribe(() => this.bizyLongPress.emit());
+  @HostListener('mouseup')
+  @HostListener('mouseleave')
+  @HostListener('touchend')
+  @HostListener('touchcancel')
+  onPressEnd(): void {
+    this.clearTimeout();
+  }
+
+  private clearTimeout(): void {
+    if (this.#pressTimeout) {
+      clearTimeout(this.#pressTimeout);
+      this.#pressTimeout = null;
+    }
   }
 
   ngOnDestroy(): void {
-    if (this.#event) {
-      this.#event.unsubscribe();
-    }
+    this.clearTimeout();
   }
 }
