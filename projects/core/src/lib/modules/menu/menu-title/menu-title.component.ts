40742138ba8dca55f2f3751e26b3fb9b0d30fa51
Angelo Raso <angeloraso6@gmail.com>
2025-06-17 18:58:06 -0300

    Adjust menu title

--- projects/core/src/lib/modules/menu/menu-title/menu-title.component.ts
+++ projects/core/src/lib/modules/menu/menu-title/menu-title.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-
 @Component({
   selector: 'bizy-menu-title',
   templateUrl: './menu-title.html',
   styleUrls: ['./menu-title.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { 
+    '[id]': 'id',
+    '[class]': 'customClass'
+  }
 })
 export class BizyMenuTitleComponent {
+  @Input() id: string = `bizy-menu-title-${Math.random()}`;
   @Input() customClass: string = '';
 }
