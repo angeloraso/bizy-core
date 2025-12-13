7ee34c47321c706b9339a34b1d8622e1b521e433
Angelo Raso <araso@anura.com.ar>
2025-12-13 16:39:38 -0300

    Add readonly property to checkbox component

--- projects/core/src/lib/modules/checkbox/checkbox.component.ts
+++ projects/core/src/lib/modules/checkbox/checkbox.component.ts
@@ -11,23 +11,23 @@ import { FormsModule } from '@angular/forms';
 })
 export class BizyCheckboxComponent {
   readonly #elementRef = inject(ElementRef);
-
   @Input() id: string = `bizy-checkbox-${Math.random()}`;
   @Input() selected: boolean = false;
   @Input() disabled: boolean = false;
+  @Input() readonly: boolean = false;
   @Output() selectedChange = new EventEmitter<boolean>();
   @Output() onSelect = new EventEmitter<PointerEvent>();
 
   _checkboxId = `bizy-input-checkbox-${Math.random()}`;
 
+  getNativeElement = () => this.#elementRef?.nativeElement;
+
   _onSelect(event: PointerEvent) {
-    if (this.disabled) {
+    if (this.disabled || this.readonly) {
       return;
     }
 
     this.selectedChange.emit(!this.selected);
     this.onSelect.emit(event)
   }
-
-  getNativeElement = () => this.#elementRef?.nativeElement;
 }
