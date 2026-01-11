808158b523835e7fb26ff4feca617f0149e75314
Angelo Raso <araso@anura.com.ar>
2026-01-11 11:52:09 -0300

    Fix filter component css variables

--- projects/core/src/lib/modules/filter/filter.component.ts
+++ projects/core/src/lib/modules/filter/filter.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren, EventEmitter, Input, Output, QueryList, DOCUMENT, ElementRef, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren, EventEmitter, Input, Output, QueryList, DOCUMENT, ElementRef, inject, ViewChild } from '@angular/core';
 import { BizyFilterSectionComponent } from './filter-section/filter-section.component';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { BizyFilterSectionsComponent } from './filter-sections/filter-sections.component';
+import { CdkConnectedOverlay } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'bizy-filter',
@@ -20,6 +21,7 @@ export class BizyFilterComponent {
   readonly #elementRef = inject(ElementRef);
   readonly #document = inject(DOCUMENT);
   readonly #ref = inject(ChangeDetectorRef);
+  @ViewChild(CdkConnectedOverlay) overlay!: CdkConnectedOverlay;
   @ContentChildren(BizyFilterSectionComponent, { descendants: true }) private sections: QueryList<BizyFilterSectionComponent>;
   @Input() id: string = `bizy-filter-${Math.random()}`;
   @Input() disabled: boolean = false;
@@ -76,6 +78,30 @@ export class BizyFilterComponent {
     }
 
     this.onOpen.emit(event);
+
+    this.#ref.detectChanges();
+    const panel = this.overlay.overlayRef?.overlayElement;
+    if (panel) {
+      const maxWidth = this.#getClosestCssVariable(this.#elementRef.nativeElement, '--bizy-filter-max-width')!;
+      panel.style.setProperty('--bizy-filter-max-width', maxWidth);
+
+      const sectionMinWidth = this.#getClosestCssVariable(this.#elementRef.nativeElement, '--bizy-filter-section-min-width')!;
+      panel.style.setProperty('--bizy-filter-section-min-width', sectionMinWidth);
+
+      const inputMinWidth = this.#getClosestCssVariable(this.#elementRef.nativeElement, '--bizy-input-min-width')!;
+      panel.style.setProperty('--bizy-input-min-width', inputMinWidth);
+
+      const inputWidth = this.#getClosestCssVariable(this.#elementRef.nativeElement, '--bizy-input-width')!;
+      panel.style.setProperty('--bizy-input-width', inputWidth);
+
+      const inputMaxWidth = this.#getClosestCssVariable(this.#elementRef.nativeElement, '--bizy-input-max-width')!;
+      panel.style.setProperty('--bizy-input-max-width', inputMaxWidth);
+
+      const inputBackgroundColor = this.#getClosestCssVariable(this.#elementRef.nativeElement, '--bizy-input-background-color')!;
+      panel.style.setProperty('--bizy-input-background-color', inputBackgroundColor);
+
+      this.#ref.detectChanges();
+    }
   }
 
   close = (event: PointerEvent & {target: {id: string}}) => {
@@ -88,6 +114,21 @@ export class BizyFilterComponent {
   }
 
   getNativeElement = () => this.#elementRef?.nativeElement;
+
+  #getClosestCssVariable = (element: HTMLElement, cssVariable: string): string | null => {
+    while (element) {
+      const value = getComputedStyle(element).getPropertyValue(cssVariable).trim();
+      if (value) {
+        return value;
+      }
+
+      element = element.parentElement as HTMLElement;
+    }
+
+    const rootValue = getComputedStyle(this.#document.documentElement).getPropertyValue(cssVariable).trim();
+    return rootValue || null;
+  }
+
 
   ngOnDestroy() {
     this.#subscription.unsubscribe();
