a76538eb28d68a13103830c02db26fada0196084
Angelo Raso <araso@anura.com.ar>
2026-04-02 12:05:02 -0300

    Add windowSizeChange$ observable

--- projects/core/src/lib/services/device/device.service.ts
+++ projects/core/src/lib/services/device/device.service.ts
@@ -1,10 +1,31 @@
 import { inject, Injectable } from '@angular/core';
 import { getUserAgent } from './uach-retrofill';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { debounceTime, fromEvent, map, Observable, startWith } from 'rxjs';
+
+interface WindowSize {
+  height: number,
+  width: number
+}
 
 @Injectable({ providedIn: 'root' })
 export class BizyDeviceService {
   readonly #device = inject(DeviceDetectorService);
+
+  get windowSizeChange$(): Observable<WindowSize> {
+    return fromEvent(window, 'resize').pipe(
+      debounceTime(200),
+      map((event: any) => <WindowSize>{
+        width: event.currentTarget.innerWidth,
+        height: event.currentTarget.innerHeight
+      }),
+      startWith({
+          width: window.innerWidth,
+          height: window.innerHeight
+      })
+    );
+  }
+
 
   async getUserAgent() {
     try {
