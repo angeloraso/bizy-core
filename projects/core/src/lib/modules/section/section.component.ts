55daf4e37b940b4c2bf468ab5b4cd233b418ec5b
Angelo Raso <araso@anura.com.ar>
2026-04-02 12:29:35 -0300

    Add selection event to section components

--- projects/core/src/lib/modules/section/section.component.ts
+++ projects/core/src/lib/modules/section/section.component.ts
@@ -1,5 +1,5 @@
 
-import { ChangeDetectionStrategy, Component, ElementRef, inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, inject, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'bizy-section',
@@ -8,12 +8,17 @@ import { ChangeDetectionStrategy, Component, ElementRef, inject, Input } from '@
   imports: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { 
-    '[id]': 'id'
+    '[id]': 'id',
+    '(click)': 'onSelect.emit($event)',
+    '(keydown.enter)': 'onSelect.emit($event)',
+    'tabindex': '0',
+    'role': 'button'
   }
 })
 export class BizySectionComponent {
   readonly #elementRef = inject(ElementRef);
   @Input() id: string = `bizy-section-${Math.random()}`;
+  @Output() onSelect = new EventEmitter<PointerEvent>();
 
   getNativeElement = () => this.#elementRef?.nativeElement;
 }
